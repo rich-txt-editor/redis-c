b53b16e7e42c73c970bcdc649991f953bfa53f41
John <4448701+jrmol@users.noreply.github.com>
2024-07-05 10:03:53 -0400

    protocol parsing

--- server.cpp
+++ server.cpp
@@ -7,6 +7,17 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <assert.h>
+
+/*
++-----+------+-----+------+--------
+| len | msg1 | len | msg2 | more...
++-----+------+-----+------+--------
+The protocol consists of 2 parts: a 4-byte little-endian integer
+indicating the length of the request and the variagble-length of
+the request.
+
+*/
 
 static void msg(const char *msg)
 {
@@ -20,19 +31,91 @@ static void die(const char *msg)
     abort();
 }
 
-static void do_something(int connfd)
+const size_t k_max_msg = 4096;
+
+// helper funciton that reads from the kernel until it gets exactly n bytes.
+static int32_t read_full(int fd, char *buf, size_t n)
 {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
+    while (n > 0)
+    {
+        // read() syslcall returns whatever data is available in kernel, or blocks if there is none
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1; // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        // may return successfully with partial data if kernel buf is full
+        // we must keep trying when the write() returns fewer bytes than we need.
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1; // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// one_request does the heavy work
+static int32_t one_request(int connfd)
+{
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            msg("EOF");
+        }
+        else
+        {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); // assume little endian
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err)
     {
         msg("read() error");
-        return;
+        return err;
     }
-    printf("client says: %s\n", rbuf);
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main()
@@ -43,7 +126,7 @@ int main()
         die("socket()");
     }
 
-    // needed for most server apps
+    // this is needed for most server apps
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
@@ -76,7 +159,15 @@ int main()
             continue; // error
         }
 
-        do_something(connfd);
+        while (true)
+        {
+            // here, the server only serves one client connection at a time
+            int32_t err = one_request(connfd);
+            if (err)
+            {
+                break;
+            }
+        }
         close(connfd);
     }
 
